8c952db1371c82a54d8d2b395bb8fa003d40ce87
Creator <creator@gitlab.com>
2020-01-18 20:44:11 +0000

    Trying to get the image from the request.

--- forms.py
+++ forms.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, FileField
 from wtforms.validators import DataRequired, Length
 
 class UPCForm(FlaskForm):
+    upc = StringField('UPC number', validators=[DataRequired(), Length(max=12)])
+    image = FileField('Image File')
+    submit = SubmitField('Look up')
+
+class UPCFileForm(FlaskForm):
+    image = FileField('Image File', validators=[DataRequired()])
+    submit = SubmitField('Look up')
+
+class UPCStringForm(FlaskForm):
     upc = StringField('UPC number', validators=[DataRequired(), Length(max=12)])
     submit = SubmitField('Look up')
