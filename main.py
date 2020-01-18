43014136b901ccb0cba9998b6243e5c2054ca8bc
Zhe Xuan Chua <bluefish6543@gmail.com>
2020-01-18 22:23:53 +0000

    Improve history page

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from forms import UPCForm
 from config import Config
+import history
 import requests
+import os
 import db
 
 app = Flask(__name__)
@@ -34,8 +36,15 @@ def main():
         else:
             # Do something with the label and calories data
             pass
-            
+
     return render_template('index.html', title='Carbon footprint calculator', form=form)
+
+@app.route('/history')
+def show_history():
+    history.plot()
+    table_data = history.sort_types()
+    filename = os.path.join(app.config['UPLOAD_FOLDER'], 'tmp.png')
+    return render_template('history.html', image=filename, table_data=table_data)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
