74478b6c55584e54ca3e86dd37629cf21c0c7096
James Thompson <jhrt2@cam.ac.uk>
2020-01-19 07:32:28 +0000

    weekly average print added

--- history.py
+++ history.py
@@ -43,3 +43,33 @@ def sort_types():
     
     sorted_data = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
     return sorted_data
+
+def weekly_average(today): #today = when 'today' is, either actually today or one week ago
+    dates = []
+    week_mean = 0
+    for i in reversed(range(7)):
+        date = today - datetime.timedelta(i)
+        dates.append(str(date))
+        data = db.get_data_by_date(date.year, date.month, date.day)
+        for entity in data:
+            week_mean += float(entity['footprint'])
+    week_mean = week_mean/7
+    return week_mean
+
+def weekly_improvement():
+    this_week = weekly_average(datetime.date.today()) #today
+    last_week = weekly_average(datetime.date.today() - datetime.timedelta(7)) #one week ago
+
+    if last_week == 0.0:
+        return 'No data for previous week.'
+
+    percentage_difference = (this_week - last_week)*100/last_week
+    
+    if percentage_difference >= 0:
+        return "You're carbon footprint increased by " + str(percentage_difference) + "%" + " compared to last week."
+    else:
+        return "You're carbon footprint decreased by " + str(percentage_difference) + "%" + " compared to last week."
+    
+
+
+print(weekly_improvement())
